1d1e201ac6b84c0f3ba6a001a124bdcaaf43807b
davidadduru <adduru.davidpaul@auf.edu.ph>
2024-08-26 23:12:04 +0800

    index.php

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
     <meta charset="utf-8">
     <title>IPT10 Laboratory Activity #2</title>
@@ -21,26 +22,54 @@
   <div class="col">
     <h4>File Upload</h4>
 
-    <form>
+    <form action="index.php" method="post" enctype="multipart/form-data">
         <div class="p-card">
-            <h3>Text File</h3>
+            <h3>PDF File</h3>
             <p class="p-card__content">
-            <input type="file" name="text_file" accept=".txt" />
+            <input type="file" id="pdf-file" name="pdfFile" accept=".pdf" required />
             </p>
         </div>
 
         <div>
-            <button>
-                Upload
-            </button>
+            <button type="submit">Upload</button>
         </div>
     </form>
-    </div>
+  </div>
   <div class="col">
-  <img class="p-logo-section__logo" src="https://www.auf.edu.ph/home/images/mascot/CCS.png" alt="College of Computing Studies">
+    <img class="p-logo-section__logo" src="https://www.auf.edu.ph/home/images/mascot/CCS.png" alt="College of Computing Studies">
   </div>
 </div>
 
+<?php
+if (isset($_FILES['pdfFile'])) {
+    $fileName = $_FILES['pdfFile']['name'];
+    $fileTmp = $_FILES['pdfFile']['tmp_name'];
+    $fileType = $_FILES['pdfFile']['type'];
+
+    // Ensure the uploaded file is a PDF
+    if ($fileType == "application/pdf") {
+        $uploadDir = "uploads/";
+
+        // Ensure the uploads directory exists
+        if (!is_dir($uploadDir)) {
+            mkdir($uploadDir, 0777, true);
+        }
+
+        $filePath = $uploadDir . basename($fileName);
+
+        if (move_uploaded_file($fileTmp, $filePath)) {
+            echo "<div id='pdf-display' style='margin-top: 20px;'>";
+            echo "<h4>Uploaded PDF:</h4>";
+            echo "<iframe id='pdf-frame' src='$filePath' style='width:100%; height:600px;' frameborder='0'></iframe>";
+            echo "</div>";
+        } else {
+            echo "<p style='color:red;'>Error uploading file.</p>";
+        }
+    } else {
+        echo "<p style='color:red;'>Please upload a valid PDF file.</p>";
+    }
+}
+?>
+
 </body>
 </html>
-
