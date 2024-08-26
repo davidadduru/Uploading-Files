cb8bc2365b708db4cd9a6279a822f1fcc9ea2f4c
davidadduru <adduru.davidpaul@auf.edu.ph>
2024-08-26 23:09:57 +0800

    uploaded.php

--- uploaded.php
+++ uploaded.php
@@ -1,22 +1,24 @@
 <?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $targetDir = "uploads/";
+    $targetFile = $targetDir . basename($_FILES["audioFile"]["name"]);
+    $uploadOk = 1;
+    $audioFileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
 
-$upload_directory = getcwd() . '/uploads/';
-$relative_path = '/uploads/';
+    if ($audioFileType != "mp3") {
+        echo "Sorry, only MP3 files are allowed.";
+        $uploadOk = 0;
+    }
 
-// Handle Text File
-$uploaded_text_file = $upload_directory . basename($_FILES['text_file']['name']);
-$temporary_file = $_FILES['text_file']['tmp_name'];
-
-if (move_uploaded_file($temporary_file, $uploaded_text_file)) {
-    $text_file_content = file_get_contents($uploaded_text_file, 'r');
-    ?>
-    <textarea cols="70" rows="30"><?php echo $text_file_content; ?></textarea>
-    <?php
-} else {
-    echo 'Failed to upload file';
+    if ($uploadOk == 0) {
+        echo "Sorry, your file was not uploaded.";
+    } else {
+        if (move_uploaded_file($_FILES["audioFile"]["tmp_name"], $targetFile)) {
+            header("Location: index.php?file=" . urlencode($targetFile));
+            exit();
+        } else {
+            echo "Sorry, there was an error uploading your file.";
+        }
+    }
 }
-
-
-echo '<pre>';
-var_dump($_FILES);
-exit;
+?>
